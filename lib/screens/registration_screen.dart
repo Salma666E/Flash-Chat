4fc166208deed80692b1d5eb17f88f05cf8c9794
Salma Essam <smsmacs666@gmail.com>
2022-03-21 16:12:02 +0200

    build routes app

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+  static String id = 'Registration';
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
